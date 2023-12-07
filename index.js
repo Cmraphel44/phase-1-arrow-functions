c8f79de416da8c6d01292f4d411b05e09bea1a0d
Christopher <raphelchris@gmail.com>
2023-12-07 15:20:17 -0800

    complete

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide() {
+    return 2000/100;
+
+}
+
+ const square = (parameter) => parameter * parameter;
+ square(2);
+
+
+const add = (number1, number2) => number1 + number2;
+add(3,4);
+
